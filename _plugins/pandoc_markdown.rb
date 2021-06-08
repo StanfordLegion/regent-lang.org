0e03048fb86e5e19e8bb7d38812c72a926e62846
Elliott Slaughter <slaughter@cs.stanford.edu>
2021-06-08 14:06:35 -0700

    Add Pandoc plugin.

---
+++ _plugins/pandoc_markdown.rb
@@ -0,0 +1,19 @@
+# From https://github.com/elliottslaughter/jekyll-pandoc-plugin
+module Jekyll
+  module Converters
+    class Markdown
+      class Pandoc
+        def initialize(config)
+          Jekyll::External.require_with_graceful_fail "pandoc-ruby"
+          @config = config["pandoc"] || {}
+        end
+
+        def convert(content)
+          extensions = @config['extensions'] || []
+          format = @config['format'] || 'html5'
+          ::PandocRuby.new(content, *extensions).send("to_#{format}")
+        end
+      end
+    end
+  end
+end
